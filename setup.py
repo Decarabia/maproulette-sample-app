d73c34b3c226da36e89c524093deac500386b332
paulGelbach <paul.gelbach@critigen.com>
2020-03-20 13:02:42 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setup(name="sample_app",
+      version="0.0.1",
+      description="A Python Sample Application",
+      long_description=long_description,
+      url="https://github.com/osmlab/maproulette-python-client",
+      packages=find_packages('maproulette'),
+      python_requires='>=3.6',
+      setup_requires=["pytest-runner"],
+      tests_require=["pytest"],
+      package_dir={'': 'maproulette'},
+      install_requires=['requests'])
